1ed8db7d48bbaadfe3e3fe971234404f7fcb9858
Connor <connorjwalton2@gmail.com>
2021-06-17 10:43:33 -0600

    Button

--- src/components/Portfolio/Portfolio-contain.js
+++ src/components/Portfolio/Portfolio-contain.js
@@ -5,10 +5,18 @@ export default class Portfoliocontainer extends Component {
         super();
 this.state = {
     pageTitle:"This is my portfolio",
-    data: [{title: "Lorem" }, {title:"Ipsum"},{title: "Sit"},{title: "Amet"}]
+    data: [{title: "Lorem", category: "Test1"}, {title:"Ipsum", category:"Test2"},{title: "Sit",category:"Test3"},{title: "Amet",category:"Test4"}]
 }
-this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
+this.handleFilter=this.handleFilter.bind(this);
     }
+    
+    handleFilter(filter){
+        this.setState({
+        data: this.state.data.filter(items => {
+            return items.category === filter;
+        })
+         })
+}
     Items(){
     return this.state.data.map(items => {
         return <Item title={items.title}/>;
@@ -19,9 +27,11 @@ this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
             <div>
                 <h2>Insert content here</h2>
                 {this.Item()}
-                <hr/>
-                <button onClick={this.handlePageTitleUpdate}>Change the name</button>
-            </div>
+                <button onClick={() => this.handleFilter('Test1')}>Test1</button>
+                <button onClick={() => this.handleFilter('Test2')}>Test2</button>
+                <button onClick={() => this.handleFilter('Test3')}>Test3</button>
+                <button onClick={() => this.handleFilter('Test4')}>Test4</button>
+                </div>
         )
     }
 }
