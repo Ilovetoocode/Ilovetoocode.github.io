7075c20e549c648878307608d8e9ff5ff38f2144
Connor <connorjwalton2@gmail.com>
2021-06-17 10:23:02 -0600

    More

--- src/components/Portfolio/Portfolio-contain.js
+++ src/components/Portfolio/Portfolio-contain.js
@@ -4,19 +4,28 @@ export default class Portfoliocontainer extends Component {
     constructor(){
         super();
 this.state = {
+    pageTitle:"This is my portfolio",
     data: [{title: "Lorem" }, {title:"Ipsum"},{title: "Sit"},{title: "Amet"}]
 }
+this.handlePageTitleUpdate = this.handlePageTitleUpdate.bind(this);
     }
     Items(){
     return this.state.data.map(items => {
         return <Item title={items.title}/>;
     })
     }
+    handlePageTitleUpdate(){
+        this.setState({
+            pageTitle:"AAAAAAAAAAAA"
+        })
+    }
     render(){
         return (
             <div>
                 <h2>Insert content here</h2>
                 {this.Item()}
+                <hr/>
+                <button onClick={this.handlePageTitleUpdate}>Change the name</button>
             </div>
         )
     }
