2dcb685f961bac0cb7172a8764a350f37a3075a8
Connor <connorjwalton2@gmail.com>
2021-06-17 09:38:57 -0600

    Finished

--- src/components/Portfolio/Portfolio-contain.js
+++ src/components/Portfolio/Portfolio-contain.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react";
 import Item from "./Item";
 export default class Portfoliocontainer extends Component {
+    constructor(){
+        super();
+
+        console.log("The container is ready");
+    }
     render(){
         return (
             <div>
                 <h2>Insert content here</h2>
-                <item />
+                <Item />
             </div>
         )
     }
