e764387a741be7e67c4e76e04ecd182cf8c55ad9
Connor <connorjwalton2@gmail.com>
2021-06-17 09:34:57 -0600

    Functionality

---
+++ src/components/Portfolio/Item.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Portfolioitem from "./item";
+export default function() {
+    return(
+       <div> 
+           <h3>Items</h3>
+           </div>
+    )
+}
--- src/components/Portfolio/Portfolio-contain.js
+++ src/components/Portfolio/Portfolio-contain.js
@@ -1,10 +1,11 @@
 import React, { Component } from "react";
-
+import item from "./item";
 export default class Portfoliocontainer extends Component {
     render(){
         return (
             <div>
-                
+                <h2>Insert content here</h2>
+                <item />
             </div>
         )
     }
