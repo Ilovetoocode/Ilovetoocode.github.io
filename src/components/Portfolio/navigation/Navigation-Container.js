5573e6e542665535a1b7caa0e9f5af3648cb33c9
Connor <connorjwalton2@gmail.com>
2021-06-17 11:00:23 -0600

    More stuff

---
+++ src/components/Portfolio/navigation/Navigation-Container.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+export default class NavigationComponent extends Component{
+    constructor(){
+        super();
+    }
+    render(){
+    return(
+    <div>
+        <button>Home</button>
+        <button>Contact</button>
+        <button>About</button>
+        <button>Blog</button>
+        <button>Add blog</button>
+        <button>Nothing button </button>
+    </div>
+    );
+    }
+}
--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import moment from "moment";
 import Portfoliocontainer from './Portfolio/Portfolio-contain';
+import NavigationContainer from './navigation/Navigation-Container';
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
+      <NavigationContainer />
         <h1>The geek crowd</h1>
         <div>
         {moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
