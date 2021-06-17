7868b0052a4b1d4d956de1ef1bcbffe369b8ba75
Connor <connorjwalton2@gmail.com>
2021-06-17 11:25:22 -0600

    Some updates

--- src/components/app.js
+++ src/components/app.js
@@ -1,12 +1,24 @@
 import React, { Component } from 'react';
 import moment from "moment";
+import {
+  BrowserRouter as Router,Switch, Route} from "react-router-dom"
 import Portfoliocontainer from './Portfolio/Portfolio-contain';
 import NavigationContainer from './navigation/Navigation-Container';
+import home from './Pages/home';
+import about from './Pages/about';
 export default class App extends Component {
   render() {
     return (
       <div className='app'>
-      <NavigationContainer />
+      <Router>
+        <div>
+        <NavigationContainer />
+       <Switch>
+         <Route exact path="/" component={home} />
+         <Route path="/About-me" component={about} />
+       </Switch>
+        </div>
+      </Router>
         <h1>The geek crowd</h1>
         <div>
         {moment().format('MMMM Do YYYY, h:mm:ss a')}</div>
