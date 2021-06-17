b28114bcbd7c769c2f9bad20ce570cee4a2097e7
Connor <connorjwalton2@gmail.com>
2021-06-17 09:13:22 -0600

    New

---
+++ src/components/Portfolio-contain.js
@@ -0,0 +1,11 @@
+import React, { Component } from "react";
+
+export default class Portfoliocontainer extends Component {
+    render(){
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
