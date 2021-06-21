bef61201401c620bf8b5250e28fff401f3ce2051
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2021-06-21 09:02:27 -0600

    Added some extra API functinality code

--- src/components/Portfolio/Portfolio-contain.js
+++ src/components/Portfolio/Portfolio-contain.js
@@ -1,15 +1,14 @@
 import React, { Component } from "react";
 import Item from "./Item";
+
 import axios from "axios";
+
 export default class Portfoliocontainer extends Component {
     constructor(){
         super();
 this.state = {
     pageTitle:"This is my portfolio",
-    data: [{title: "Neocities nonesense", category: "Web-stuff", slug: 'neocities'}, 
-    {title:"My paintings", category:"Physical", slug:"Paint"},
-    {title: "Youtube",category:"Web stuff", slug:"Video"},
-    {title: "Eagle award",category:"Accomplishment", slug:"Eagle"}]
+    data: []
 }
 this.handleFilter=this.handleFilter.bind(this);
 this.getPortfolioItems=this.getPortfolioItems.bind(this)
@@ -22,7 +21,6 @@ this.getPortfolioItems=this.getPortfolioItems.bind(this)
         })
          })
 }
-
 getPortfolioItems(){
     axios
     .get("https://whoami.devcamp.space/portfolio/portfolio_items")
@@ -38,18 +36,27 @@ getPortfolioItems(){
         return <Item title={items.title} slug={items.slug}/>;
     })
     }
+    portfolioitems(){
+        return this.state.data.map(Item => {
+            return(
+                <porfolioitem title={Item.title} slug={Item.slug} />
+            );
+        });
+    }
+    componentDidMount(){
+        this.getPortfolioItems();
+    }
     render(){
         if (this.state.isLoading){
             return <div>Loading...</div>
         }
-        this.getPortfolioItems();
         return (
             <div>
                 <h2>Some stuff I have done</h2>
                 {this.Items()}
                 <button onClick={() => this.handleFilter('Physical')}>Physical</button>
                 <button onClick={() => this.handleFilter('Web stuff')}>Web stuff</button>
-                <button onClick={() => this.handleFilter('Test3')}>Test3</button>
+                <button onClick={() => this.handleFilter('Accomplishment')}>Awards</button>
                 <button onClick={() => this.handleFilter('Test4')}>Test4</button>
                 </div>
         )
