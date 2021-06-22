c73e74f41ef4e43c43c91d5d7b87b9a8f4065aae
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2021-06-22 11:18:16 -0600

    Landmine one dodged

--- src/components/auth/Lgn.js
+++ src/components/auth/Lgn.js
@@ -9,9 +9,11 @@ export default class Login extends Component{
         this.handleChange=this.handleChange.bind(this);
         this.handleSubmit=this.handleSubmit.bind(this);
     }
-    
+
     handleChange(event){
-        console.log("Handle change", event)
+        this.setState({
+            [event.target.name]: event.target.value,
+        })
     }
 
     handleSubmit(event){
