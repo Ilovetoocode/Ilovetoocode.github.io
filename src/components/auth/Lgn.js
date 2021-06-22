d02c2ee274893bc44caaa942a48c26e6fc341984
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2021-06-22 11:02:40 -0600

    Treading lightly

--- src/components/auth/Lgn.js
+++ src/components/auth/Lgn.js
@@ -1,5 +1,12 @@
 import React, {Component} from "react";
 export default class Login extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            email:"",
+            password:"",
+        }
+    }
     render(){
     return (
         <div>
