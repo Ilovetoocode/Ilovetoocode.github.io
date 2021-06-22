dc1385b340d1a20d4fd3cb594ba83adb62114bc3
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2021-06-22 11:22:57 -0600

    Axios import and no more console log.... last commit until this thing works

--- src/components/auth/Lgn.js
+++ src/components/auth/Lgn.js
@@ -1,4 +1,5 @@
 import React, {Component} from "react";
+import axios from 'axios';
 export default class Login extends Component{
     constructor(props){
         super(props)
@@ -17,7 +18,7 @@ export default class Login extends Component{
     }
 
     handleSubmit(event){
-        console.log("Handle submit", event);
+        
         event.preventDefault();
     }
     render(){
