618c30484ba8f6b4737f58ad056171981d500a43
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2021-06-23 08:59:25 -0600

    Debug code in here, ready for a new giant monster section that could break stuff

--- src/components/app.js
+++ src/components/app.js
@@ -33,6 +33,7 @@ export default class App extends Component {
     return (
       <div className='Contain'>
       <NavigationContainer />
+      <h2>{this.state.loggedInState}</h2>
       <Router>
         <div>
        <Switch>
