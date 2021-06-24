b723d81b09c0f71903b5dd889004086c8b6055c7
Ilovetoocode <connorjwalton2@gmail.com>
2021-06-24 17:24:44 -0600

    Stylization

--- src/components/Portfolio/form.js
+++ src/components/Portfolio/form.js
@@ -21,10 +21,28 @@ export default class Portform extends Component{
         this.componentConfig=this.componentConfig.bind(this);
         this.djsConfig=this.djsConfig.bind(this)
         this.hndlthmbdrp=this.hndlthmbdrp.bind(this)
+        this.handlelogodrop=this.handlelogodrop.bind(this)
+        this.handlebannerdrop=this.handlebannerdrop.bind(this)
+        this.thumbref = React.createRef();
+        this.logoref = React.createRef();
+        this.bannerref = React.createRef();
     }
     Submit(event){
         axios.post("https://whoami.devcamp.space/portfolio/portfolio_items",this.buildForm(),{withCredentials:true}).then(response => {
             this.props.handleformsubmit(response.data.portfolio_item);
+            this.setState({
+                name:"",
+                description:"",
+                category:"Accomplishment",
+                position:"",
+                url:"",
+                thumb_image:"",
+                banner_image:"",
+                logo:"",
+            });
+            [this.thumbref, this.bannerref, this.logoref].forEach(ref =>{
+                ref.current.dropzone.removeAllFiles();
+            })
         }).catch(error =>{
             console.log("An error occoured", error)
         })
@@ -34,6 +52,16 @@ export default class Portform extends Component{
         return{
             addedfile: file => this.setState({thumb_image:file})
         };
+    }
+    handlelogodrop(){
+        return{
+            addedfile: file => this.setState({logo:file})
+        }
+    }
+    handlebannerdrop(){
+        return{
+            addedfile: file => this.setState({banner_image:file})
+        }
     }
     componentConfig(){
         return{
@@ -58,6 +86,12 @@ export default class Portform extends Component{
         if(this.state.thumb_image){
             formData.append("portfolio_item[thumb_image]", this.state.thumb_image);
         }
+        if(this.state.banner_image){
+            formData.append("portfolio_item[banner_image]", this.state.banner_image);
+        }
+        if(this.state.logo){
+            formData.append("portfolio_item[logo]", this.state.logo);
+        }
         return formData;
     }
     Changer(event){
@@ -67,38 +101,39 @@ export default class Portform extends Component{
     }
     render(){
     return (
-        <div>
-            <h1>form</h1>
-            <form onSubmit={this.Submit}>
-                <div>
+            <form onSubmit={this.Submit} className="portfoliomanaging">
+                <div className="two-collumn">
                 <input 
                     type="text"
-                    name="position"
-                    placeholder="pos"
-                    value={this.state.position}
-                    onChange={this.Changer}/>
-                    <input 
-                    type="text"
                     name="name"
-                    placeholder="portitem"
+                    placeholder="Title"
                     value={this.state.name}
                     onChange={this.Changer}/>
-                    <select 
-                    name="category"
-                    value={this.state.category}
-                    onChange={this.Changer}>
-                    <option value="Accomplishment">Accomplishment</option>
-                    <option value="Real life">IRL</option>
-                    <option value="Web Stuff">Web Stuff</option>
-                    </select>
-                    <input 
+                      <input 
                     type="text"
                     name="url"
                     placeholder="Link"
                     value={this.state.url}
                     onChange={this.Changer}/>
                 </div>
-                <div>
+                <div className="two-collumn">
+                    <select 
+                    name="category"
+                    value={this.state.category}
+                    onChange={this.Changer}
+                    className="select-element">
+                    <option value="Accomplishment">Accomplishment</option>
+                    <option value="Real life">IRL</option>
+                    <option value="Web Stuff">Web Stuff</option>
+                    </select>
+                    <input 
+                    type="text"
+                    name="position"
+                    placeholder="pos"
+                    value={this.state.position}
+                    onChange={this.Changer}/>
+                </div>
+                <div className="one-collumn">
                 <textarea 
                     type="text"
                     name="description"
@@ -106,11 +141,24 @@ export default class Portform extends Component{
                     value={this.state.description}
                     onChange={this.Changer}/>
                 </div>
-                <div className="img-Upload">
+                <div className="img-Upload three-collumn">
                 <DropzoneComponent 
+                ref={this.thumbref}
                 config={this.componentConfig()}
                 djsConfig={this.djsConfig()}
                 eventHandlers={this.hndlthmbdrp()}
+                />
+               < DropzoneComponent 
+               ref={this.bannerref}
+                config={this.componentConfig()}
+                djsConfig={this.djsConfig()}
+                eventHandlers={this.handlebannerdrop()}
+                />
+                < DropzoneComponent 
+                ref={this.logoref}
+                config={this.componentConfig()}
+                djsConfig={this.djsConfig()}
+                eventHandlers={this.handlelogodrop()}
                 />
                 </div>
                 <div>
@@ -119,7 +167,6 @@ export default class Portform extends Component{
                         </button>
                     </div>
             </form>
-        </div>
     )
     }
 }
