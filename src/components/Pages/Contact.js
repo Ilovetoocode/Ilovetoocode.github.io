397199a4e6c4a7b6615f5596d6cd35905bc92d25
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2021-07-08 11:58:32 -0600

    Styling

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import mail from'../../../static/assets/images/contact/mail.jpg'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function(){
     return(
         <div 
@@ -13,7 +14,14 @@ export default function(){
         }}
         />
         <div className="bottom-row">
-Nice rows here
+        <h1><FontAwesomeIcon icon="comment"/> Discord: @Mr.Meow#9999</h1>
+        <h1><a href="https://twitter.com/loldisizawsum" className="twitter-link">
+            <FontAwesomeIcon icon= "hashtag"/>
+            </a>My Twitter</h1>
+            <h1><FontAwesomeIcon className="mailing"icon="paper-plane"/>
+            Email: Connorjwalton2@gmail.com
+            </h1>
+            <h1><FontAwesomeIcon icon="blender-phone"/> Phone number:contact me through other means for more details</h1>
         </div>
         </div>
     )
