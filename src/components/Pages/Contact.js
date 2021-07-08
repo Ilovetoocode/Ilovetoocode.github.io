18270cab508b15f3e289c6f70ba27322a94c715a
Ilovetoocode <46007308+Ilovetoocode@users.noreply.github.com>
2021-07-08 11:30:40 -0600

    Styles starting!

--- src/components/Pages/Contact.js
+++ src/components/Pages/Contact.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import mail from'../../../static/assets/images/contact/mail.jpg'
 export default function(){
     return(
-        <div>
-        Contact
+        <div 
+        className="contentpage"
+        >
+        <div className="top-row"
+        style={{
+            background:"url("+mail+") no-repeat",
+            backgroundSize:"100%",
+            backgroundPosition:"center",
+        }}
+        />
+        <div className="bottom-row">
+Nice rows here
+        </div>
         </div>
     )
 }
--- src/components/Pages/about.js
+++ src/components/Pages/about.js
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import mascot from'../../../static/assets/images/Bio/Mrmeow.jpg'
 export default function(){
     return(
-        <div>
-        About
+        <div 
+        className="contentpage"
+        >
+        <div className="top-row"
+        style={{
+            background:"url("+mascot+") no-repeat",
+            backgroundSize:"100%",
+            backgroundPosition:"center",
+        }}
+        />
+        <div className="bottom-row">
+Nice rows here
+        </div>
         </div>
     )
 }
